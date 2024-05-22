b36b13c89ee2b7d9ae24c8b44c2f393c0bdb38d2
JackMizh <47821665+JackMizh@users.noreply.github.com>
2024-05-22 18:03:39 +0700

    Set-up Room dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,5 +68,8 @@ dependencies {
 
     implementation(libs.androidx.activity.ktx)
 
+    implementation(libs.androidx.room.runtime)
+    kapt(libs.androidx.room.compiler)
 
+    implementation(libs.androidx.room.ktx)
 }
