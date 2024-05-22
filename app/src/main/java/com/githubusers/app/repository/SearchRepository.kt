bc18fceaf9206bac180ae9780de1175fe6607d56
JackMizh <47821665+JackMizh@users.noreply.github.com>
2024-05-22 16:48:35 +0700

    Set-Up Repository for search API

---
+++ app/src/main/java/com/githubusers/app/repository/SearchRepository.kt
@@ -0,0 +1,21 @@
+package com.githubusers.app.repository
+
+import com.githubusers.app.api.GithubApi
+import com.githubusers.app.data.UserListResponse
+import retrofit2.Response
+import javax.inject.Inject
+
+class SearchRepository @Inject constructor(
+    private val githubApi: GithubApi
+) {
+    suspend fun searchUser(keyword: String): Response<UserListResponse>
+    {
+        val response = githubApi.searchUser(keyword)
+        if(response.isSuccessful){
+
+        }else{
+
+        }
+        return response
+    }
+}
